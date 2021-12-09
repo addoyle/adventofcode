054b6b8aba2a23bf336948e626e02c4ce9a6d030
Andy Doyle <andy.doyle@inovexcorp.com>
2021-12-09 01:57:22 -0500

    Completing day 8

--- day8/pt2.js
+++ day8/pt2.js
@@ -1,31 +1,12 @@
 const common = require('../common');
 
-const eight = Array.from(Array(7).keys());
-const indexMap = [
-  [0, 1, 2, 4, 5, 6], // 0
-  [2, 5], // 1
-  [0, 2, 3, 4, 6], // 2
-  [0, 2, 3, 5, 6], // 3
-  [1, 2, 3, 5], // 4
-  [0, 1, 3, 5, 6], // 5
-  [0, 1, 3, 4, 5, 6], // 6
-  [0, 2, 5], // 7
-  [...eight], // 8
-  [0, 1, 2, 3, 5, 6] // 9
-].map((index, digit) => ({
-  digit,
-  add: index,
-  subtract: eight.filter(n => !index.includes(n))
-}));
-
 const getDigitMap = patterns => {
   const union = (a, b) => [...new Set(a.concat(b))];
   const diff = (elementsIn, butNotIn) =>
     elementsIn.filter(i => !butNotIn.includes(i));
-  const intersection = (a, b) => a.filter(i => b.includes(i));
-  // const subset = (a, containsB) => 
+  const intersect = (a, b) => a.filter(i => b.includes(i));
+  const subset = (a, containsB) => intersect(a, containsB).length === containsB.length;
 
-  const digits = {};
   const patternsByLength = patterns
     .split(' ')
     .map(pattern => pattern.split('').sort())
@@ -39,97 +20,47 @@ const getDigitMap = patterns => {
       {}
     );
 
+  const digitMap = {};
+
   // Assign known patterns to numbers
-  const one = patternsByLength[2][0];
-  const four = patternsByLength[4][0];
-  const seven = patternsByLength[3][0];
-  const eight = patternsByLength[7][0];
+  digitMap[1] = patternsByLength[2][0];
+  digitMap[4] = patternsByLength[4][0];
+  digitMap[7] = patternsByLength[3][0];
+  digitMap[8] = patternsByLength[7][0];
 
-  // We can deduce which pattern represents 3 as it contains all of 1
-  const 
+  // Deduce remaining numbers
 
-  // Top line is easiest, 7 - 1
-  digits[0] = diff(seven, one)[0];
+  // 3 contains 1
+  digitMap[3] = patternsByLength[5].filter(pattern => subset(pattern, digitMap[1]))[0];
+  // 5 unioned with 1 contains 4
+  digitMap[5] = patternsByLength[5].filter(pattern => subset(union(pattern, digitMap[1]), digitMap[4]))[0];
+  // 2 is the only 5-length left
+  digitMap[2] = diff(patternsByLength[5], [digitMap[3], digitMap[5]])[0];
 
-  
+  // 9 contains 4
+  digitMap[9] = patternsByLength[6].filter(pattern => subset(pattern, digitMap[4]))[0];
+  // 0 contains 1, and isn't nine
+  digitMap[0] = patternsByLength[6].filter(pattern => subset(pattern, digitMap[1]) && pattern !== digitMap[9])[0];
+  // 6 is what's left
+  digitMap[6] = diff(patternsByLength[6], [digitMap[9], digitMap[0]])[0];
 
-  console.log(digits);
+  return Object.values(digitMap).reduce((map, digit, i) => ({...map, [digit.join('')]: i}), {});
 };
 
-// const getDigitMap = patterns => {
-//   const possibles = patterns
-//     .split(' ')
-//     // .sort((a, b) => b.length - a.length)
-//     // .slice(0, 1)
-//     .reduce(
-//       (hits, pattern) => {
-//         // console.log(hits);
+console.log(
+  common.lines('input').reduce((sum, entry) => {
+    const [signalPattern, output] = entry.split('|').map(s => s.trim());
+    const digitMap = getDigitMap(signalPattern);
+    
+    return (
+      sum +
+      +output
+        .trim()
+        .split(' ')
+        .map(d => digitMap[d.split('').sort().join('')])
+        .join('')
+    );
+  }, 0)
+);
 
-//         const letters = pattern.split('');
-//         indexMap
-//           .filter(i => i.add.length === letters.length)
-//           .forEach(index => {
-//             index.add.forEach(a =>
-//               letters.forEach(l => hits[index.digit][a][l]++)
-//             );
-//             // index.subtract.forEach(s =>
-//             //   letters.forEach(l => hits[index.digit][s][l]--)
-//             // );
-//           });
-
-//         return hits;
-//       },
-//       indexMap.reduce(
-//         (segs, index, n) => ({
-//           ...segs,
-//           [n]: eight.reduce(
-//             (o, i) => ({
-//               ...o,
-//               [i]: 'abcdefg'.split('').reduce((o, i) => ({ ...o, [i]: 0 }), {})
-//             }),
-//             {}
-//           )
-//         }),
-//         {}
-//       )
-//     );
-
-//   console.log(possibles);
-
-//   // console.log({ eight });
-
-//   // // Build map
-//   // return indexMap.reduce(
-//   //   (map, index, i) => ({
-//   //     ...map,
-//   //     [index
-//   //       .map(j => eight[j])
-//   //       .sort()
-//   //       .join('')]: i
-//   //   }),
-//   //   {}
-//   // );
-// };
-
-// console.log(
-common.lines('small').reduce((sum, entry) => {
-  const [signalPattern, output] = entry.split('|');
-  // console.log('Inputs:', signalPattern, output);
-  const digitMap = getDigitMap(signalPattern.trim());
-  // console.log(digitMap);
-  // return (
-  //   sum +
-  //   +output
-  //     .trim()
-  //     .split(' ')
-  //     .map(d => {
-  //       const k = d.split('').sort().join('');
-  //       // console.log(k, digitMap[k]);
-  //       return digitMap[k];
-  //     })
-  //     .join('')
-  // );
-}, 0);
-// );
-
-// Correct answer:
+// Correct answer: 1012089
