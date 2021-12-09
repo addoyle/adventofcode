a6f843b26aec7e8756ed266e539e779175dbc4b6
adoyle <andy.d.doyle@gmail.com>
2021-12-09 00:30:58 -0500

    Finally feel good about this approach

--- day8/pt1.js
+++ day8/pt1.js
@@ -1,10 +1,15 @@
 const common = require('../common');
 
-console.log(common.lines('input').reduce((sum, entry) => 
-    sum + entry.split('|')
+console.log(
+  common.lines('input').reduce(
+    (sum, entry) =>
+      sum +
+      entry
+        .split('|')
         .map(v => v.trim().split(' '))[1]
         .filter(digit => digit.length !== 5 && digit.length !== 6).length,
     0
-));
+  )
+);
 
 // Correct answer: 274
--- day8/pt2.js
+++ day8/pt2.js
@@ -1,37 +1,135 @@
 const common = require('../common');
 
+const eight = Array.from(Array(7).keys());
 const indexMap = [
-    [0, 1, 2, 4, 5, 6],     // 0
-    [2, 5],                 // 1
-    [0, 2, 3, 4, 6],        // 2
-    [0, 2, 3, 5, 6, 7],     // 3
-    [1, 2, 3, 5],           // 4
-    [0, 1, 3, 5, 6],        // 5
-    [0, 1, 3, 4, 5, 6],     // 6
-    [0, 2, 5],              // 7
-    [0, 1, 2, 3, 4, 5, 6],  // 8
-    [0, 1, 2, 3, 5, 6]      // 9
-];
-const getDigitMap = signalPattern => {
-    // Start by finding the 8, i.e. 7 digit input
-    const eight = signalPattern.split(' ').filter(i => i.length === 7)[0].split('');
+  [0, 1, 2, 4, 5, 6], // 0
+  [2, 5], // 1
+  [0, 2, 3, 4, 6], // 2
+  [0, 2, 3, 5, 6], // 3
+  [1, 2, 3, 5], // 4
+  [0, 1, 3, 5, 6], // 5
+  [0, 1, 3, 4, 5, 6], // 6
+  [0, 2, 5], // 7
+  [...eight], // 8
+  [0, 1, 2, 3, 5, 6] // 9
+].map((index, digit) => ({
+  digit,
+  add: index,
+  subtract: eight.filter(n => !index.includes(n))
+}));
 
-    // Build map
-    return indexMap.reduce((map, index, i) => ({...map, [index.map(j => eight[j]).sort().join('')]: i}), {});
+const getDigitMap = patterns => {
+  const union = (a, b) => [...new Set(a.concat(b))];
+  const diff = (elementsIn, butNotIn) =>
+    elementsIn.filter(i => !butNotIn.includes(i));
+  const intersection = (a, b) => a.filter(i => b.includes(i));
+  // const subset = (a, containsB) => 
+
+  const digits = {};
+  const patternsByLength = patterns
+    .split(' ')
+    .map(pattern => pattern.split('').sort())
+    .reduce(
+      (pbl, pattern) => ({
+        ...pbl,
+        [pattern.length]: pbl[pattern.length]
+          ? [...pbl[pattern.length], pattern]
+          : [pattern]
+      }),
+      {}
+    );
+
+  // Assign known patterns to numbers
+  const one = patternsByLength[2][0];
+  const four = patternsByLength[4][0];
+  const seven = patternsByLength[3][0];
+  const eight = patternsByLength[7][0];
+
+  // We can deduce which pattern represents 3 as it contains all of 1
+  const 
+
+  // Top line is easiest, 7 - 1
+  digits[0] = diff(seven, one)[0];
+
+  
+
+  console.log(digits);
 };
 
-console.log(common.lines('sample').reduce(
-    (sum, entry) => {
-        const [signalPattern, output] = entry.split('|');
-        const digitMap = getDigitMap(signalPattern);
-        console.log(digitMap);
-        return sum + +output.trim().split(' ').map(d => {
-            const k = d.split('').sort().join('');
-            console.log(k);
-            return digitMap[k];
-        }).join('');
-    },
-    0
-));
+// const getDigitMap = patterns => {
+//   const possibles = patterns
+//     .split(' ')
+//     // .sort((a, b) => b.length - a.length)
+//     // .slice(0, 1)
+//     .reduce(
+//       (hits, pattern) => {
+//         // console.log(hits);
 
-// Correct answer: 
+//         const letters = pattern.split('');
+//         indexMap
+//           .filter(i => i.add.length === letters.length)
+//           .forEach(index => {
+//             index.add.forEach(a =>
+//               letters.forEach(l => hits[index.digit][a][l]++)
+//             );
+//             // index.subtract.forEach(s =>
+//             //   letters.forEach(l => hits[index.digit][s][l]--)
+//             // );
+//           });
+
+//         return hits;
+//       },
+//       indexMap.reduce(
+//         (segs, index, n) => ({
+//           ...segs,
+//           [n]: eight.reduce(
+//             (o, i) => ({
+//               ...o,
+//               [i]: 'abcdefg'.split('').reduce((o, i) => ({ ...o, [i]: 0 }), {})
+//             }),
+//             {}
+//           )
+//         }),
+//         {}
+//       )
+//     );
+
+//   console.log(possibles);
+
+//   // console.log({ eight });
+
+//   // // Build map
+//   // return indexMap.reduce(
+//   //   (map, index, i) => ({
+//   //     ...map,
+//   //     [index
+//   //       .map(j => eight[j])
+//   //       .sort()
+//   //       .join('')]: i
+//   //   }),
+//   //   {}
+//   // );
+// };
+
+// console.log(
+common.lines('small').reduce((sum, entry) => {
+  const [signalPattern, output] = entry.split('|');
+  // console.log('Inputs:', signalPattern, output);
+  const digitMap = getDigitMap(signalPattern.trim());
+  // console.log(digitMap);
+  // return (
+  //   sum +
+  //   +output
+  //     .trim()
+  //     .split(' ')
+  //     .map(d => {
+  //       const k = d.split('').sort().join('');
+  //       // console.log(k, digitMap[k]);
+  //       return digitMap[k];
+  //     })
+  //     .join('')
+  // );
+}, 0);
+// );
+
+// Correct answer:
