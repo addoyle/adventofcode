04e1581adb7b792c60e7a5bca79a3d8452a0ac22
Andy Doyle <andy.doyle@inovexcorp.com>
2021-12-11 08:54:47 -0500

    Updating common to not care about trailing line

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 module.exports = {
-    lines: f => fs.readFileSync(f, 'utf8').split('\n'),
+    lines: f => fs.readFileSync(f, 'utf8').trim().split('\n'),
     intLines: f => module.exports.lines(f).map(v => parseInt(v))
 }
