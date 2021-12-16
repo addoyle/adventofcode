34d9dd1b2d5bc937d01060954557b28ee5e10833
adoyle <andy.d.doyle@gmail.com>
2021-12-16 09:33:52 -0500

    Starting on day 16

---
+++ day16/pt1.js
@@ -0,0 +1,92 @@
+const common = require('../common.js');
+
+const rawBits = common
+  .lines('sample1')[0]
+  .split('')
+  .map(c => parseInt(c, 16).toString(2).padStart(4, '0'))
+  .join('');
+
+const TYPE_LITERAL = 4;
+
+const getPackets = bits => {
+  let i = 0;
+
+  const packets = [];
+  let packet = {};
+  let parentPacket = {};
+
+  while (i < bits.length) {
+    console.log(parentPacket);
+    // Rest of the bits are 0, nothing left to do
+    if (parseInt(bits.slice(i), 2) === 0) {
+      break;
+    }
+
+    // No version yet, grab next 3 bits
+    if (!packet.version) {
+      if (
+        parentPacket.version &&
+        parentPacket.subPackets.length <= parentPacket.total
+      ) {
+        parentPacket.subPackets.push(packet);
+      }
+      packet.version = parseInt(bits.slice(i, i + 3), 2);
+      i += 3;
+    }
+
+    // No type yet, grab next 3 bits
+    if (!packet.type) {
+      packet.type = parseInt(bits.slice(i, i + 3), 2);
+      i += 3;
+
+      if (packet.type === TYPE_LITERAL) {
+        packet.groups = [];
+        packet.hasMore = true;
+      }
+      // Anything else, operator
+      else {
+        const newPacket = {};
+        if (parentPacket.version) {
+          parentPacket.subPackets.push(newPacket);
+        }
+        parentPacket = { ...packet };
+        packet = newPacket;
+        parentPacket.subPackets = [];
+        parentPacket.isNumPackets = +bits[i++];
+
+        // Next 11 bits contains the number of packets
+        if (parentPacket.isNumPackets) {
+          console.log('numPackets');
+          parentPacket.total = parseInt(bits.slice(i, i + 11), 2);
+          i += 11;
+        }
+        // Next 15 bits represents the length of bits containing packets
+        else {
+          const length = parseInt(bits.slice(i, i + 15), 2);
+          i += 15;
+          parentPacket.subPackets = getPackets(bits.slice(i, i + length));
+          packets.push(parentPacket);
+          i += length;
+        }
+      }
+    }
+
+    // Grab next group
+    if (packet.type === TYPE_LITERAL && packet.hasMore) {
+      packet.hasMore = +bits[i++];
+      packet.groups.push(bits.slice(i, i + 4));
+      i += 4;
+
+      if (!packet.hasMore) {
+        packet.value = parseInt(packet.groups.join(''), 2);
+        packet = {};
+      }
+    }
+  }
+
+  return packets;
+};
+
+console.log(getPackets(rawBits));
+
+// Correct answer:
