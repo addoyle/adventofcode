8df19977194a7cc89e373baab282beb0b35cf00f
adoyle <andy.d.doyle@gmail.com>
2022-12-06 08:27:45 -0500

    Completed day 6

---
+++ day06/pt1.mjs
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+
+const n = 4;
+
+console.log(
+  (stream => {
+    const arr = stream.split('');
+    for (let i = 0; i < arr.length - n; i++) {
+      if (new Set(arr.slice(i, i + n)).size === n) {
+        return i + n;
+      }
+    }
+    return -1;
+  })(readFileSync('./input.txt', 'utf8'))
+);
+
+// Answer: 1544
