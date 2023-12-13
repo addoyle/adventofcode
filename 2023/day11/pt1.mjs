f1acc600741c75d5ef7bde44673aba27d1c0e148
Andy Doyle <andy.d.doyle@gmail.com>
2023-12-13 00:06:29 -0500

    Completing day 11

--- 2023/day11/pt1.mjs
+++ 2023/day11/pt1.mjs
@@ -1,140 +1,42 @@
 import { lines } from '../../common.mjs';
 
-const universe = lines('./sample.txt').map(row => row.split(''));
-universe[0].forEach((_, i) => {
-  if (universe.map(row => row[i]).every(c => c === '.')) {
-    universe.forEach(row => (row[i] = ['.', '.']));
+const universe = lines('./input.txt').map(row => row.split(''));
+
+const galaxies = [];
+universe.forEach((row, y) =>
+  row.forEach((space, x) => space === '#' && galaxies.push({ x, y, orig: [y, x], paths: [] }))
+);
+
+const expandFactor = 1;
+
+// Expand y
+universe.forEach((row, i) => {
+  if (row.every(c => c === '.')) {
+    galaxies.filter(g => g.orig[0] > i).forEach(g => (g.y += expandFactor));
   }
 });
-const expandedUniverse = universe
-  .map(row => row.flat())
-  .map(row => (row.every(c => c === '.') ? [row, row] : [row]))
-  .flat();
+// Expand x
+universe[0].forEach((_, i) => {
+  if (universe.map(row => row[i]).every(c => c === '.')) {
+    galaxies.filter(g => g.orig[1] > i).forEach(g => (g.x += expandFactor));
+  }
+});
 
-const galaxies = {};
-expandedUniverse.forEach((row, y) =>
-  row.forEach((space, x) => space === '#' && (galaxies[`${y},${x}`] = { coord: [y, x], steps: 0 }))
+galaxies.forEach(
+  src =>
+    (src.paths = galaxies
+      .filter(dest => !(dest.x === src.x && dest.y === src.y))
+      .map(dest => ({ dest, steps: Math.abs(src.x - dest.x) + Math.abs(src.y - dest.y) })))
 );
 
-
-
-let generation = Object.entries(galaxies).reduce(
-  (o, [key, galaxy]) => ({
-    ...o,
-    [key]: {
-      coord: galaxy.coord,
-      nw: o[key]?.nw ? o[key].nw.add(key) : new Set([key]),
-      ne: o[key]?.ne ? o[key].ne.add(key) : new Set([key]),
-      sw: o[key]?.sw ? o[key].sw.add(key) : new Set([key]),
-      se: o[key]?.se ? o[key].ne.add(key) : new Set([key])
+// Remove path dupes
+galaxies.forEach((src, i) => {
+  galaxies.slice(i + 1).forEach(dest => {
+    const toDel = dest.paths.find(path => path.dest.x === src.x && path.dest.y === src.y);
+    if (toDel) {
+      toDel.steps = 0;
     }
-  }),
-  {}
-);
-
-let gen = 0;
-// const nextGen = () => {
-//   const dedupe = arr => [...new Set(arr.map(i => i.join(',')))].map(i => i.split(',').map(n => parseInt(n)));
-//   gen++;
-//   galaxies.forEach(gen => {
-//     gen.nw = dedupe(
-//       gen.nw
-//         .map(step => [
-//           [step[0] - 1, step[1]],
-//           [step[0], step[1] - 1]
-//         ])
-//         .flat()
-//         .filter(step => step[0] >= 0 && step[1] >= 0)
-//     );
-//     gen.ne = dedupe(
-//       gen.ne
-//         .map(step => [
-//           [step[0] - 1, step[1]],
-//           [step[0], step[1] + 1]
-//         ])
-//         .flat()
-//         .filter(step => step[0] >= 0 && step[1] < expandedUniverse[0].length)
-//     );
-//     gen.sw = dedupe(
-//       gen.sw
-//         .map(step => [
-//           [step[0] + 1, step[1]],
-//           [step[0], step[1] - 1]
-//         ])
-//         .flat()
-//         .filter(step => step[0] < expandedUniverse.length && step[1] >= 0)
-//     );
-//     gen.se = dedupe(
-//       gen.se
-//         .map(step => [
-//           [step[0] + 1, step[1]],
-//           [step[0], step[1] + 1]
-//         ])
-//         .flat()
-//         .filter(step => step[0] < expandedUniverse.length && step[1] < expandedUniverse[0].length)
-//     );
-//   });
-// };
-const nextGen = () => {
-  gen++;
-  const next = {};
-
-  const addStep = (step, dx, dy, dir) => {
-    if (!step[dir] || step[dir].size === 0) return;
-
-    [
-      [step.coord[0] + dy, step.coord[1]],
-      [step.coord[0], step.coord[1] + dx]
-    ]
-      .filter(
-        coord =>
-          coord[0] >= 0 && coord[1] >= 0 && coord[0] < expandedUniverse.length && coord[1] < expandedUniverse[0].length
-      )
-      .forEach(coord => {
-        const key = coord.join(',');
-        next[key] = next[key] || { coord };
-        next[key][dir] = next[key][dir] || new Set();
-        next[key][dir].add(...step[dir]);
-      });
-  };
-
-  Object.entries(generation).forEach(([_, step]) => {
-    addStep(step, -1, -1, 'nw');
-    addStep(step, -1, 1, 'ne');
-    addStep(step, 1, -1, 'sw');
-    addStep(step, 1, 1, 'se');
   });
+});
 
-  generation = next;
-};
-nextGen();
-
-while (!Object.values(galaxies).every(galaxy => galaxy.steps > 0)) {
-  Object.entries(generation)
-    .filter(([key, step]) => galaxies[key] && galaxies[key].steps === 0)
-    .forEach(([key, step]) => {});
-
-  // galaxies.forEach(galaxy =>
-  //   [...new Set(galaxy.nw.concat(galaxy.ne, galaxy.sw, galaxy.se).map(step => step.join(',')))]
-  //     .filter(step => galaxies.map(g => g.coord.join(',')).includes(step))
-  //     .filter(m => !galaxy.paths[m])
-  //     .forEach(m => (galaxy.paths[m] = gen))
-  // );
-  // console.log({
-  //   gen,
-  //   nw: galaxies.map(galaxy => galaxy.nw).flat().length,
-  //   ne: galaxies.map(galaxy => galaxy.ne).flat().length,
-  //   sw: galaxies.map(galaxy => galaxy.sw).flat().length,
-  //   se: galaxies.map(galaxy => galaxy.se).flat().length
-  // });
-  nextGen();
-}
-
-const paths = {};
-galaxies.forEach(galaxy =>
-  Object.entries(galaxy.paths).forEach(
-    path => (paths[[galaxy.coord.toString(), path[0]].toSorted().toString()] = path[1])
-  )
-);
-console.log(Object.values(paths).reduce((sum, n) => sum + n, 0));
-debugger;
+console.log(galaxies.map(g => g.paths.reduce((sum, n) => sum + n.steps, 0)).reduce((sum, n) => sum + n, 0));
--- 2023/day11/pt2.mjs
+++ 2023/day11/pt2.mjs
@@ -1,150 +1,42 @@
 import { lines } from '../../common.mjs';
 
-const ground = lines('./input.txt').map(line => line.split(''));
-const loop = [];
+const universe = lines('./input.txt').map(row => row.split(''));
 
-// Find starting location
-ground.forEach((row, y) => row.forEach((pipe, x) => pipe === 'S' && loop.push([y, x])));
-
-const is = (a, b) => a[0] === b[0] && a[1] === b[1];
-
-const printLoop = () => {
-  const grid = ground.map(row => row.map(_ => '.'));
-  loop.forEach(pos => (grid[pos[0]][pos[1]] = ground[pos[0]][pos[1]]));
-  return grid;
-};
-
-// Find first valid neighbor
-loop.push(
-  [
-    [loop[0][0] - 1, loop[0][1], 'F7|'],
-    [loop[0][0], loop[0][1] + 1, 'J7-'],
-    [loop[0][0] + 1, loop[0][1], 'JL|'],
-    [loop[0][0], loop[0][1] + 1, 'FL-']
-  ].find(n => ground[n[0]] && ground[n[0]][n[1]] && n[2].includes(ground[n[0]][n[1]]))
+const galaxies = [];
+universe.forEach((row, y) =>
+  row.forEach((space, x) => space === '#' && galaxies.push({ x, y, orig: [y, x], paths: [] }))
 );
-loop[1].pop();
 
-while (true) {
-  const cur = loop.at(-1);
-  const pipe = ground[cur[0]][cur[1]];
-  if (pipe === 'S') {
-    loop.pop();
-    break;
+const expandFactor = 1000000;
+
+// Expand y
+universe.forEach((row, i) => {
+  if (row.every(c => c === '.')) {
+    galaxies.filter(g => g.orig[0] > i).forEach(g => (g.y += expandFactor - 1));
   }
-
-  loop.push(
-    (pipe === 'F'
-      ? [
-          [cur[0] + 1, cur[1]],
-          [cur[0], cur[1] + 1]
-        ]
-      : pipe === '7'
-        ? [
-            [cur[0] + 1, cur[1]],
-            [cur[0], cur[1] - 1]
-          ]
-        : pipe === 'J'
-          ? [
-              [cur[0] - 1, cur[1]],
-              [cur[0], cur[1] - 1]
-            ]
-          : pipe === 'L'
-            ? [
-                [cur[0] - 1, cur[1]],
-                [cur[0], cur[1] + 1]
-              ]
-            : pipe === '|'
-              ? [
-                  [cur[0] - 1, cur[1]],
-                  [cur[0] + 1, cur[1]]
-                ]
-              : pipe === '-'
-                ? [
-                    [cur[0], cur[1] - 1],
-                    [cur[0], cur[1] + 1]
-                  ]
-                : []
-    ).find(connection => !is(connection, loop.at(-2)))
-  );
-}
-
-// Convert S to its pipe
-(() => {
-  const s = loop[0];
-  const f = loop[1];
-  const e = loop.at(-1);
-  // start/first on same row
-  if (s[0] === f[0]) {
-    // left connection, J7- depending on end
-    if (s[1] > f[1]) {
-      ground[s[0]][s[1]] = s[0] > e[0] ? 'J' : s[0] < e[0] ? '7' : '-';
-    }
-    // right connection, LF- depending on end
-    else {
-      ground[s[0]][s[1]] = s[0] > e[0] ? 'L' : s[0] < e[0] ? 'F' : '-';
-    }
+});
+// Expand x
+universe[0].forEach((_, i) => {
+  if (universe.map(row => row[i]).every(c => c === '.')) {
+    galaxies.filter(g => g.orig[1] > i).forEach(g => (g.x += expandFactor - 1));
   }
-  // different row, assumed same col if valid input
-  else {
-    // bottom connection, JL| depending on end
-    if (s[0] > f[0]) {
-      ground[s[0]][s[1]] = s[1] > e[1] ? 'J' : s[1] < e[1] ? 'L' : '|';
-    }
-    // top connection, 7F} depending on end
-    else {
-      ground[s[0]][s[1]] = s[1] > e[1] ? '7' : s[1] < e[1] ? 'F' : '|';
-    }
-  }
-})();
+});
 
-// Pad space to loop
-const expandRow = row => [
-  row[0],
-  ...row
-    .slice(1)
-    .map(pipe => ['7J-'.includes(pipe) ? '-' : '.', pipe])
-    .flat()
-];
-const extendPipe = pipe => ['JL|'.includes(pipe) ? '|' : '.'];
-const paddedLoop = (pl => [
-  expandRow(pl[0]),
-  ...pl
-    .slice(1)
-    .map(row => [
-      [
-        extendPipe(row[0]),
-        ...row
-          .slice(1)
-          .map(pipe => ['.', extendPipe(pipe)].flat())
-          .flat()
-      ],
-      expandRow(row)
-    ])
-    .flat()
-])(printLoop());
-
-// console.log(paddedLoop.map(row => row.join('')).join('\n'));
-
-// Start at the top-most, left-most corner
-const start = (minY => loop.filter(p => p[0] === minY))(loop.reduce((minY, p) => Math.min(minY, p[0]), Infinity))
-  .reduce((min, p) => (p[1] < min[1] ? p : min), [0, Infinity])
-  .map(i => i * 2 + 1);
-const fillLoop = (cur = start) => {
-  if (paddedLoop[cur[0]][cur[1]] !== '.') {
-    return;
-  }
-  paddedLoop[cur[0]][cur[1]] = '#';
-  fillLoop([cur[0] - 1, cur[1]]);
-  fillLoop([cur[0] + 1, cur[1]]);
-  fillLoop([cur[0], cur[1] - 1]);
-  fillLoop([cur[0], cur[1] + 1]);
-};
-fillLoop();
-
-console.log(
-  paddedLoop
-    .filter((_, i) => i % 2 === 0)
-    .map(row => row.filter((pipe, i) => i % 2 === 0 && pipe === '#').length)
-    .reduce((sum, n) => sum + n, 0)
+galaxies.forEach(
+  src =>
+    (src.paths = galaxies
+      .filter(dest => !(dest.x === src.x && dest.y === src.y))
+      .map(dest => ({ dest, steps: Math.abs(src.x - dest.x) + Math.abs(src.y - dest.y) })))
 );
+
+// Remove path dupes
+galaxies.forEach((src, i) => {
+  galaxies.slice(i + 1).forEach(dest => {
+    const toDel = dest.paths.find(path => path.dest.x === src.x && path.dest.y === src.y);
+    if (toDel) {
+      toDel.steps = 0;
+    }
+  });
+});
+
+console.log(galaxies.map(g => g.paths.reduce((sum, n) => sum + n.steps, 0)).reduce((sum, n) => sum + n, 0));
