4693695bd6b4af9837ae4e02059c4d7feb622e0a
Andy Doyle <andy.d.doyle@gmail.com>
2023-12-14 23:34:59 -0500

    Day12 pt1 is done, pt2 needs to be completed

--- 2023/day12/pt1.mjs
+++ 2023/day12/pt1.mjs
@@ -1,9 +1,8 @@
 import { lines } from '../../common.mjs';
 
-const springs = lines('./sample.txt')
+const springs = lines('./input.txt')
   .map(line => line.split(' '))
   .map(spring => ({
-    // record: spring[0].match(/(#+)|(\?+)|(\.+)/g),
     record: spring[0],
     groups: spring[1].split(',').map(n => parseInt(n)),
     perms: 0,
@@ -13,20 +12,25 @@ const springs = lines('./sample.txt')
 const buildPerm = (spring, perm = '', r = 0, g = 0) => {
   if (r >= spring.record.length) {
     // is valid?
-    if (!perm.includes('?') && g >= spring.groups.length) {
+    if (
+      !perm.includes('?') &&
+      g >= spring.groups.length &&
+      perm.length === spring.record.length &&
+      [...spring.record].every((c, i) => c !== '#' || c === perm[i])
+    ) {
       spring.perms++;
       spring.tempPerms.push(perm);
     }
     return;
   }
 
-  const grp = spring.groups[g];
-  const rec = spring.record.substring(r, r + grp);
-
   // operational, keep going
-  if (rec[0] === '.') {
+  if (spring.record[r] === '.') {
     return buildPerm(spring, perm + '.', r + 1, g);
   }
+
+  const grp = spring.groups[g];
+  const rec = spring.record.substring(r, r + grp);
 
   // got a broken size that fits our group
   if (rec === '#'.repeat(grp)) {
@@ -35,46 +39,63 @@ const buildPerm = (spring, perm = '', r = 0, g = 0) => {
       return;
     }
 
-    // If next char is a ?, mark as operational
-    const next = spring.record.charAt(r + grp) === '?';
-
-    // got our first match, keep going
-    return buildPerm(spring, perm + rec + (next ? '.' : ''), r + grp + (next ? 1 : 0), g + 1);
+    const atEnd = r + grp >= spring.record.length;
+    return buildPerm(spring, perm + rec + (atEnd ? '' : '.'), r + grp + (atEnd ? 0 : 1), g + 1);
   }
 
   const block = spring.record.substring(r).match(/^[?#]+/)[0];
 
-  // block starts with # and won't get too big
-  if (block[0] === '#' && block[grp] !== '#') {
-    return buildPerm(spring, perm + '#'.repeat(grp) + '.', r + grp + 1, g + 1);
-  }
-
-  // block ends with a #, check if there's more #'s after it
-  if (block[grp - 1] === '#') {
-    const firstBrkn = block.indexOf('#');
-    const fromBrkn = block.substring(firstBrkn).match(/^#+/)[0];
-    // make sure it's not too big
-    if (fromBrkn.length <= grp) {
-      // same size, mark as broken and continue
-      if (fromBrkn.length === grp) {
-        buildPerm(spring, perm + '.'.repeat(grp - firstBrkn) + fromBrkn + '.', r + grp + firstBrkn + 2, g + 1);
-      }
-      // smaller, add possibilities to stack
-      else {
-        for (let i = r; i < Math.min(block.length - grp, grp - firstBrkn) - 1; i++) {
-          buildPerm(spring, perm + '.'.repeat(i) + '#'.repeat(grp) + '.', r + grp + i + 1, g + 1);
-        }
-      }
-    }
+  // block size matches group
+  if (block.length === grp) {
+    // Try with and without
+    buildPerm(spring, perm + '#'.repeat(grp), r + grp, g + 1);
+    buildPerm(spring, perm + '.'.repeat(grp), r + grp, g);
     return;
   }
 
+  // no groups left
+  if (g >= spring.groups.length) {
+    // all ?'s left, successful perm
+    if ([...block].every(c => c === '?')) {
+      return buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
+    }
+    // #'s left, invalid
+    else {
+      return;
+    }
+  }
+
+  // block is too small
+  if (grp > block.length) {
+    // all ?'s, replace with .'s
+    if ([...block].every(c => c === '?')) {
+      return buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
+    }
+    // contains a #, invalid
+    else {
+      return;
+    }
+  }
+
   // Some combination of ?'s and #'s left in block
-  for (let i = r; i < block.length - grp; i++) {
-    buildPerm(spring, perm + '.'.repeat(i) + '#'.repeat(grp) + '.', r + grp + i + 1, g + 1);
+  for (let i = 0; i <= block.length - grp; i++) {
+    const atEnd = r + grp + i >= spring.record.length;
+    if (block[i + grp] !== '#' && block[i - 1] !== '#') {
+      buildPerm(
+        spring,
+        perm + '.'.repeat(i) + '#'.repeat(grp) + (atEnd ? '' : '.'),
+        r + grp + i + (atEnd ? 0 : 1),
+        g + 1
+      );
+    }
+  }
+
+  // Lastly, try skipping the block entirely, if all ?'s
+  if ([...block].every(c => c === '?')) {
+    buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
   }
 };
 
 springs.forEach(spring => buildPerm(spring));
-
+console.log(springs.reduce((sum, spring) => sum + spring.perms, 0));
 debugger;
--- 2023/day12/pt2.mjs
+++ 2023/day12/pt2.mjs
@@ -1,42 +1,137 @@
 import { lines } from '../../common.mjs';
 
-const universe = lines('./input.txt').map(row => row.split(''));
+const permutator = (record, groups) =>
+  [
+    [record, groups],
+    [record + '?', groups],
+    [Array(2).fill(record).join('?'), Array(2).fill(groups).flat()],
+    [Array(3).fill(record).join('?'), Array(3).fill(groups).flat()],
+    [Array(4).fill(record).join('?'), Array(4).fill(groups).flat()],
+    [Array(5).fill(record).join('?'), Array(5).fill(groups).flat()]
+  ].map(([record, groups]) => ({ record, groups, perms: 0, tempPerms: [] }));
+const springs = lines('./input.txt')
+  .map(line => line.split(' '))
+  .map(([record, groups]) =>
+    permutator(
+      record,
+      groups.split(',').map(n => parseInt(n))
+    )
+  );
 
-const galaxies = [];
-universe.forEach((row, y) =>
-  row.forEach((space, x) => space === '#' && galaxies.push({ x, y, orig: [y, x], paths: [] }))
-);
-
-const expandFactor = 1000000;
-
-// Expand y
-universe.forEach((row, i) => {
-  if (row.every(c => c === '.')) {
-    galaxies.filter(g => g.orig[0] > i).forEach(g => (g.y += expandFactor - 1));
-  }
-});
-// Expand x
-universe[0].forEach((_, i) => {
-  if (universe.map(row => row[i]).every(c => c === '.')) {
-    galaxies.filter(g => g.orig[1] > i).forEach(g => (g.x += expandFactor - 1));
-  }
-});
-
-galaxies.forEach(
-  src =>
-    (src.paths = galaxies
-      .filter(dest => !(dest.x === src.x && dest.y === src.y))
-      .map(dest => ({ dest, steps: Math.abs(src.x - dest.x) + Math.abs(src.y - dest.y) })))
-);
-
-// Remove path dupes
-galaxies.forEach((src, i) => {
-  galaxies.slice(i + 1).forEach(dest => {
-    const toDel = dest.paths.find(path => path.dest.x === src.x && path.dest.y === src.y);
-    if (toDel) {
-      toDel.steps = 0;
+const buildPerm = (spring, perm = '', r = 0, g = 0) => {
+  if (r >= spring.record.length) {
+    // is valid?
+    if (
+      !perm.includes('?') &&
+      g >= spring.groups.length &&
+      perm.length === spring.record.length &&
+      [...spring.record].every((c, i) => c !== '#' || c === perm[i])
+    ) {
+      spring.perms++;
+      spring.tempPerms.push(perm);
     }
-  });
-});
+    return;
+  }
 
-console.log(galaxies.map(g => g.paths.reduce((sum, n) => sum + n.steps, 0)).reduce((sum, n) => sum + n, 0));
+  // operational, keep going
+  if (spring.record[r] === '.') {
+    return buildPerm(spring, perm + '.', r + 1, g);
+  }
+
+  const grp = spring.groups[g];
+  const rec = spring.record.substring(r, r + grp);
+
+  // got a broken size that fits our group
+  if (rec === '#'.repeat(grp)) {
+    // but it's too big, invalid
+    if (spring.record.substring(r).match(/^#+/)[0].length > grp) {
+      return;
+    }
+
+    const atEnd = r + grp >= spring.record.length;
+    return buildPerm(spring, perm + rec + (atEnd ? '' : '.'), r + grp + (atEnd ? 0 : 1), g + 1);
+  }
+
+  const block = spring.record.substring(r).match(/^[?#]+/)[0];
+
+  // block size matches group
+  if (block.length === grp) {
+    // Try with and without
+    buildPerm(spring, perm + '#'.repeat(grp), r + grp, g + 1);
+    buildPerm(spring, perm + '.'.repeat(grp), r + grp, g);
+    return;
+  }
+
+  // no groups left
+  if (g >= spring.groups.length) {
+    // all ?'s left, successful perm
+    if ([...block].every(c => c === '?')) {
+      return buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
+    }
+    // #'s left, invalid
+    else {
+      return;
+    }
+  }
+
+  // block is too small
+  if (grp > block.length) {
+    // all ?'s, replace with .'s
+    if ([...block].every(c => c === '?')) {
+      return buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
+    }
+    // contains a #, invalid
+    else {
+      return;
+    }
+  }
+
+  // Some combination of ?'s and #'s left in block
+  for (let i = 0; i <= block.length - grp; i++) {
+    const atEnd = r + grp + i >= spring.record.length;
+    if (block[i + grp] !== '#' && block[i - 1] !== '#') {
+      buildPerm(
+        spring,
+        perm + '.'.repeat(i) + '#'.repeat(grp) + (atEnd ? '' : '.'),
+        r + grp + i + (atEnd ? 0 : 1),
+        g + 1
+      );
+    }
+  }
+
+  // Lastly, try skipping the block entirely, if all ?'s
+  if ([...block].every(c => c === '?')) {
+    buildPerm(spring, perm + '.'.repeat(block.length), r + block.length, g);
+  }
+};
+
+springs.slice(6, 7).forEach(batch => {
+  batch.forEach(spring => {
+    console.log(spring.record, spring.groups.join(','));
+    // buildPerm(spring);
+    // console.log(spring.perms);
+  });
+  debugger;
+});
+// console.log(springs.reduce((sum, spring) => sum + spring.perms, 0));
+debugger;
+
+[
+  [1, 1, 1, 1, 1, 1],
+  [4, 4, 32, 256, 2048, 16384],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 2, 4, 8, 16],
+  [4, 4, 20, 100, 500, 2500],
+  [10, 15, 150, 2250, 33750, 506250],
+  [2, 2, 4, 8, 16, 32],
+  [6, 11, 92, 1981, 54310, 1728845],
+  [10, 20, 150, 2250, 33750, 506250],
+  [2, 3, 4, 8, 16, 32],
+  [5, 5, 25, 125, 625, 3125],
+  [4, 4, 20, 100, 500, 2500],
+  [11, 22, 222, 5067, 126900, 3368871]
+].forEach(([o, p, o2, o3, o4, o5]) => {
+  const eq = o * (o2 / p) ** 4;
+  console.log({ o, p, o5, eq });
+});
+debugger;
