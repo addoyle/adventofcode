438fcaf4c523c846644e748d69a30762b9af5522
Andy Doyle <andy.d.doyle@gmail.com>
2024-12-03 00:37:35 -0500

    Completing day 3

---
+++ 2024/day03/pt1.mjs
@@ -0,0 +1,28 @@
+import { lines } from '../../common.mjs';
+
+let doIt = true;
+
+console.log(
+  lines('./input.txt')
+    .join('')
+    .match(/((?<=mul\()\d+,\d+(?=\)))|(do\(\))|(don't\(\))/g)
+    .filter(instruct => {
+      if (instruct === 'do()') {
+        doIt = true;
+      } else if (instruct === "don't()") {
+        doIt = false;
+      } else {
+        return doIt; // filter based on status of doIt
+      }
+      return false; // don't add do and don't
+    })
+    .map(mul =>
+      mul
+        .split(',')
+        .map(n => parseInt(n))
+        .reduce((prod, n) => prod * n, 1)
+    )
+    .reduce((sum, n) => sum + n, 0)
+);
+
+debugger;
