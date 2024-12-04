f44ebdb658afeb627c0d5cf264d783787b498c52
Andrew Doyle <andy.d.doyle@gmail.com>
2024-12-04 20:47:15 +0000

    A little bit more work done

--- 2021/day18/pt1.mjs
+++ 2021/day18/pt1.mjs
@@ -1,5 +1,19 @@
 import { lines } from '../../common.mjs';
 
-const homework = lines('./sample1.txt').map(row => JSON.parse(row));
-console.log(homework);
+const reduce = (num, depth = 0) => {
+  if (!Array.isArray(num)) {
+    return num;
+  }
+
+  return num.map(n => reduce(n, depth + 1));
+};
+
+const homework = lines('./sample1.txt')
+  .map(row => JSON.parse(row))
+  .reduce((sum, num) => {
+    const s = reduce([sum, num]);
+
+    return s;
+  });
+console.log(JSON.stringify(homework));
 debugger;
