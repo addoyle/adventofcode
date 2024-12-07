508493fea5ff31a0e04f9c1b6291a6023244e059
Andy Doyle <andy.d.doyle@gmail.com>
2024-12-07 10:27:11 -0500

    Completed 2024/7

---
+++ 2024/day07/pt2.mjs
@@ -0,0 +1,14 @@
+import { lines } from '../../common.mjs';
+
+console.log(
+  lines('./input.txt')
+    .map(line => line.split(': '))
+    .map(([result, terms]) => [parseInt(result), terms.split(' ').map(t => parseInt(t))])
+    .filter(([result, terms]) =>
+      terms
+        .slice(1)
+        .reduce((m, t) => m.map(v => [v + t, v * t, parseInt(`${v}${t}`)]).flat(), [terms[0]])
+        .some(n => n === result)
+    )
+    .reduce((sum, [result]) => sum + result, 0)
+);
