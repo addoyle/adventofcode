81c118549f6e53ecb105690a1ebe7bbd135adcd6
Andrew Doyle <andy.d.doyle@gmail.com>
2024-12-18 23:59:13 +0000

    More work on 16, stubbed out 18

--- 2024/day16/pt1.mjs
+++ 2024/day16/pt1.mjs
@@ -1,8 +1,10 @@
 import { ArraySet, lines } from '../../common.mjs';
 
-const grid = lines('./input.txt').map(line => line.split(''));
+const grid = lines('./sample2.txt').map(line => line.split(''));
 
 const pos = [grid.findIndex(row => row.includes('S')), grid.find(row => row.includes('S')).indexOf('S')];
+
+const moveMap = new Map();
 
 let gen = [{ visited: new ArraySet([pos]), score: 0, dir: [0, 1], pos: [...pos] }];
 let minPathScore = Infinity;
@@ -23,14 +25,51 @@ while (gen.length) {
             // Not the opposite direction
             (dir[0] === 0 || dir[0] !== -d[0]) &&
             (dir[1] === 0 || dir[1] !== -d[1])
-          // TODO: Add dead end detection
         )
         .map(newDir => {
-          const newPos = [pos[0] + newDir[0], pos[1] + newDir[1]];
-          const newScore = score + 1 + (dir[0] !== newDir[0] || dir[1] !== newDir[1] ? 1000 : 0);
+          const moveKey = [...pos, ...newDir].join(',');
+          if (!moveMap.has(moveKey)) {
+            // Take the first step
+            const cur = [pos[0] + newDir[0], pos[1] + newDir[1]];
+            // Move to next junction, turn, dead end, or maze end
+            while (
+              // Walls on both sides
+              grid[cur[0] + newDir[1]][cur[1] + newDir[0]] === '#' &&
+              grid[cur[0] - newDir[1]][cur[1] - newDir[0]] === '#'
+            ) {
+              cur[0] += newDir[0];
+              cur[1] += newDir[1];
+
+              // Got to the end!
+              if (grid[cur[0]][cur[1]] === 'E') {
+                break;
+              }
+
+              // Wall, dead end
+              if (grid[cur[0]][cur[1]] === '#') {
+                cur = null;
+                break;
+              }
+            }
+            moveMap.set(moveKey, cur);
+          }
+
+          // Null implies a dead end
+          if (moveMap.get(moveKey) === null) {
+            return null;
+          }
+
+          const newPos = moveMap.get(moveKey);
+          const newScore =
+            score +
+            // Add number of steps
+            Math.abs(newPos[0] - pos[0]) +
+            Math.abs(newPos[1] - pos[1]) +
+            // A turn was made, add 1000
+            (dir[0] !== newDir[0] || dir[1] !== newDir[1] ? 1000 : 0);
 
           // Made it to the end
-          if (grid[pos[0] + newDir[0]][pos[1] + newDir[1]] === 'E') {
+          if (grid[newPos[0]][newPos[1]] === 'E') {
             minPathScore = Math.min(minPathScore, newScore);
             return null;
           }
