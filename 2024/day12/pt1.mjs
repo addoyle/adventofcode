e09adfb4ea0d3e6007b030f063e81168be6d446f
Andy Doyle <andy.d.doyle@gmail.com>
2024-12-20 23:05:40 -0500

    Committing current state of day 12

--- 2024/day12/pt1.mjs
+++ 2024/day12/pt1.mjs
@@ -1,19 +1,23 @@
 import { ArraySet, lines } from '../../common.mjs';
 
-const garden = lines('./sample1.txt')
-  .map(line => line.split('').map(c => ({ c, plot: null })))
-  .forEach((row, y) => {
-    row.forEach((plant, x) => {
-      [
-        [y + 1, x],
-        [y - 1, x],
-        [y, x + 1],
-        [y, x - 1]
-      ]
-        .filter(n => garden[n[0]]?.[n[1]] !== undefined)
-        .forEach(n => {});
-    });
+const garden = lines('./sample1.txt').map(line => line.split('').map(c => ({ c })));
+garden.forEach((row, y) => {
+  row.forEach((plant, x) => {
+    const setN = (d, n) => {
+      if (n !== undefined) {
+        plant[d] = n;
+        n[3 - 'nwes'.indexOf(d)] = plant;
+      }
+    };
+
+    setN('n', garden[y - 1]?.[x]);
+    setN('w', garden[y][x - 1]);
+    setN('e', garden[y][x + 1]);
+    setN('s', garden[y + 1]?.[x]);
   });
+});
+
+const allPlants = new WeakSet([...garden.flat()]);
 
 const findPlots = (pt = [0, 0], plot = new ArraySet(), plots = [plot], seen = new ArraySet()) => {
   plot.add(pt);
